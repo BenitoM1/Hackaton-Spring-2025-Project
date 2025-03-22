225a68491c47a3d170d517a7c6fcdc687f326fad
nedj05 <nedjohns05@gmail.com>
2025-03-22 13:24:10 -0500

    signin2

--- hackathon_project/hackathon_project/app/page.tsx
+++ hackathon_project/hackathon_project/app/page.tsx
@@ -3,12 +3,44 @@ import { Navbar } from "./navbar";
 
 export default function Home() {
   return (
-    
-    <div>
-      <>
-      <Navbar />
-      </>
-      <h1>Test Message</h1>
-    </div>
-  );
+    <html>
+
+    <head>
+    <title> BNBJI Financing</title>
+    </head>
+
+    <body>
+      <form>
+
+      <div>
+      <label htmlFor="user">Username:</label>
+      <input type="text" id="user" placeholder=" Please Type Here"></input>
+      </div>
+
+      <div>
+      <label htmlFor="pass">Password:</label>
+      <input type="text" id="pass" placeholder=" Please Type Here"></input>
+      </div>
+
+      <div>
+      <label htmlFor="create">Create User</label>
+      <input type="button" id="create"></input>
+      </div>
+
+      <div>
+      <label htmlFor="forgot">Forgot Password</label>
+      <input type="button" id="forgot"></input>
+      </div>
+      
+      <div>
+        <input type= "submit"></input>
+      </div>
+
+      </form>
+    </body>
+
+    </html>
+
+  )
+
 }
