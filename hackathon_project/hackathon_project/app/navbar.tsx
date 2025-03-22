6485e83efdfe462250461b9c7305d8742b423488
Alonso Mendoza <benitomendoza1021@gmail.com>
2025-03-22 08:42:45 -0500

    Setting Up the navbar
    Signed-off-by: Alonso Mendoza <benitomendoza1021@gmail.com>

---
+++ hackathon_project/hackathon_project/app/navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+    return(
+    <div id="navbar">
+        <a href="#home">Home</a>
+        <a href="#add/sub">Add/Sub</a>
+        <a href="#stocks">Stocks</a>
+        <a href="#budget">Budgeting</a>
+    </div>
+    )
+}
