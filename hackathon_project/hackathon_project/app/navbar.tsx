8b78bc52012161bb00191f0c56b4a1d52ae81413
Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>
2025-03-22 11:51:24 -0500

    fixing
    Signed-off-by: Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>

--- hackathon_project/hackathon_project/app/navbar.tsx
+++ hackathon_project/hackathon_project/app/navbar.tsx
@@ -1,12 +1,21 @@
 export function Navbar() {
     return(
-    <div>
-        <nav className="flex w full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20"> </nav>
-
+    <nav className="flex w-full items-center justify-between px-[20px] py-[16px]">
+        <div>
         <a href="#home">Home</a>
+        </div>
+
+        <div>
         <a href="#add/sub">Add/Sub</a>
+        </div>
+
+        <div>
         <a href="#stocks">Stocks</a>
+        </div>
+
+        <div>
         <a href="#budget">Budgeting</a>
-    </div>
+        </div>
+    </nav>
     )
 }
