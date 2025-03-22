38e3c1bd9b6c2ee372669a08b3ecf772357da73a
juliansco214 <jsco174@lsu.edu>
2025-03-22 14:03:59 -0500

    testing3

---
+++ hackathon_project/hackathon_project/app/createaccount.tsx
@@ -0,0 +1,49 @@
+import { Html } from "next/document"
+export default function Page() {
+
+
+
+<html>
+     <head>
+         <meta charSet="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+         </meta>
+         <title>Create an Account</title>
+         <link rel="stylesheet" href="style.css">
+         </link>
+     </head>
+    <body>
+<div className="signup-container">
+    constructor(parameters) {
+        
+    }
+       <h1>Create an Account</h1>
+       <form action=" /signup" method="POST"
+       <label htmlFor="username">username</label>
+
+        const element = array[index];
+        <input 
+          type="text" id="username" name="username" required>
+     </input>
+     <label htmlFor="email">Email</label>
+    <input type="email" id="email" name="email" required 
+
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" name="password" required>
+
+      <label htmlFor="checking">Checking Account Number</label>
+      <input type="text" id="checking" name="checking" required>
+
+      <label htmlFor="savings">Savings Account Number</label>
+      <input type="text" id="savings" name="savings" required>
+
+    
+
+
+
+</div>
+    
+      </body> 
+ 
+ </html>
+}
--- hackathon_project/hackathon_project/app/page.tsx
+++ hackathon_project/hackathon_project/app/page.tsx
@@ -1,13 +1,8 @@
 import Image from "next/image";
-import { Navbar } from "./navbar";
 
 export default function Home() {
   return (
-    
     <div>
-      <>
-      <Navbar />
-      </>
       <h1>Test Message</h1>
     </div>
   );
