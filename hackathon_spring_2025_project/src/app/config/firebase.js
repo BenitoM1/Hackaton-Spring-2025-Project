2e8804c2f4b16970e1463d7a8701e8b5884c5e15
Alonso Mendoza <benitomendoza1021@gmail.com>
2025-03-21 19:00:47 -0500

    Set Up
    Signed-off-by: Alonso Mendoza <benitomendoza1021@gmail.com>

---
+++ hackathon_spring_2025_project/src/app/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtvlc_RXFLw7sTT7O1ZilCix9jlLzKdZs",
+  authDomain: "hackathon-spring-2025-project.firebaseapp.com",
+  projectId: "hackathon-spring-2025-project",
+  storageBucket: "hackathon-spring-2025-project.firebasestorage.app",
+  messagingSenderId: "378315541834",
+  appId: "1:378315541834:web:74e2374b5eb7c550b705b3"
+};
+
+const app = initializeApp(firebaseConfig);
