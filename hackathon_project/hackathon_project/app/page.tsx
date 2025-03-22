144129e3ed0e3528ee376c5784edda4e2997f3a0
Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>
2025-03-22 10:31:35 -0500

    Making seqperate files
    Signed-off-by: Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>

--- hackathon_project/hackathon_project/app/page.tsx
+++ hackathon_project/hackathon_project/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import { Navbar } from "./navbar";
 
 export default function Home() {
   return (
+    
     <div>
+      <>
+      <Navbar />
+      </>
       <h1>Test Message</h1>
     </div>
   );
