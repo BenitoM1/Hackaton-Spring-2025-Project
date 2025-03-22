0a86057b0f7a880daa309a9b5c81dc9da2bfe1dd
Brett10000 <Brettwp2300@gmail.com>
2025-03-22 16:04:36 -0500

    Update budgetguide.tsx

---
+++ hackathon_project/hackathon_project/app/budgetguide.tsx
@@ -0,0 +1,14 @@
+<html>
+<header>Budget Guide</header>
+
+<h1>It Looks Like You Might Need Help Budgeting.</h1>
+
+<h1>Here is a good option for safety.</h1>
+<a>- https://shorturl.at/NIZOD</a>
+
+<h1>Looking for something user friendly?</h1>
+<a>- https://shorturl.at/nYAw0</a>
+
+<h1>Maybe you just want the best all around!</h1>
+<a>- https://shorturl.at/TWQ2T</a>
+</html>
