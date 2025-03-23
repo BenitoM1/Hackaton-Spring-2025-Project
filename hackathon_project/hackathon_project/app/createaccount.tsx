1c2dc12411f757d62740b6971156f5596b4ddc37
juliansco214 <jsco174@lsu.edu>
2025-03-23 10:21:05 -0500

    121212

---
+++ hackathon_project/hackathon_project/app/createaccount.tsx
@@ -0,0 +1,26 @@
+export default function Page() {
+    return (
+      <div className="signup-container">
+        <h1>Create an Account</h1>
+        <form action="/signup" method="POST">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+  
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+  
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+  
+          <label htmlFor="checking">Checking Account Number</label>
+          <input type="text" id="checking" name="checking" required />
+  
+          <label htmlFor="savings">Savings Account Number</label>
+          <input type="text" id="savings" name="savings" required />
+  
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    );
+  }
+  
