dcae87ccddc321c7a8ccaa7c9de478c31e06681e
Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>
2025-03-23 10:20:07 -0500

    Update page.tsx

--- hackathon_project/hackathon_project/app/page.tsx
+++ hackathon_project/hackathon_project/app/page.tsx
@@ -1,59 +1,133 @@
-import Image from "next/image";
-import { Navbar } from "./navbar";
+"use client"
+
+import { useState } from "react";
+import Link from 'next/link';
 
 export default function Home() {
+  const [showBudgeting, setShowBudgeting] = useState(false)
+  const [showHome, setShowHome] = useState(false)
+  const [showBudgetHow, setShowBudgetHow] = useState(false)
+  const [showCreateUser, setShowCreateUser] = useState(false)
+
+  const toggleBudget = () => {
+    setShowHome(false)
+    setShowBudgetHow(false)
+    setShowBudgeting(true)
+  }
+
+  const toggleHome = () => {
+    setShowBudgeting(false)
+    setShowBudgetHow(false)
+    setShowHome(true)
+  }
+
+  const toggleBudgetHow = () => {
+    setShowBudgeting(false)
+    setShowHome(false)
+    setShowBudgetHow(true)
+  }
+
+  const toggleCreateUser = () => {
+    setShowCreateUser(true)
+  }
+
   return (
-    <>
+    <div>
+      <nav className="flex w-full items-center justify-between px-[20px] py-[16px]">
+        <div>
+          <button onClick={toggleHome} className="button-class"> Home </button>
+        </div>
 
-      <div>
-      <>
-      <Navbar />
-      </>
+        <div>
+          <Link href="/addsub" className="button-class">Add/Sub</Link>
+        </div>
+
+        <div>
+          <Link href="/stocks" className="button-class">Stocks</Link>
+        </div>
+
+        <div>
+          <button onClick={toggleBudget} className="button-class"> Budgeting </button>
+        </div>
+      </nav>
+
+      <head>
+        <title>BNBJI Financing</title>
+      </head>
+
+      <main className="main">
+        {!showBudgeting && !showHome ? (
+          <form>
+            <div>
+              <label htmlFor="user">Username:</label>
+              <input type="text" id="user" placeholder=" Please Type Here" />
+            </div>
+
+            <div>
+              <label htmlFor="pass">Password:</label>
+              <input type="password" id="pass" placeholder=" Please Type Here" />
+            </div>
+
+            <div>
+              <button onClick={toggleCreateUser} className="button-class"> Create User </button>
+            </div>
+
+            <div>
+              <label htmlFor="forgot">Forgot Password</label>
+              <button type="button" id="forgot">Forgot</button>
+            </div>
+
+            <div>
+              <input type="submit" value="Submit" />
+            </div>
+          </form>
+        ) : showBudgeting ? (
+          <div className="container">
+            <h1 className="text-2xl font-bold">Budgeting Page</h1>
+
+            <button onClick={toggleBudgetHow} className="button-class"> How To Budget & Save </button>
+
+            <div className="limits">
+              <h2>Limits</h2>
+              <select>
+                <option value="day">Day</option>
+                <option value="week">Week</option>
+                <option value="year">Year</option>
+              </select>
+            </div>
+
+            <div className="spending-goal">
+              <label htmlFor="spend">How much do you want to spend?</label>
+              <input type="number" id="spend" placeholder="$0.00" />
+            </div>
+          </div>
+        ): showHome ? (
+          <body>
+            
+          </body>
+
+        ): showBudgetHow ?(
+          <html>
+            <div>
+            <header>Budget Guide</header>
+            <h1>It Looks Like You Might Need Help Budgeting.</h1>
+
+            <h1>Here is a good option for safety.</h1>
+            <a>- https://shorturl.at/NIZOD</a>
+
+            <h1>Looking for something user friendly?</h1>
+            <a>- https://shorturl.at/nYAw0</a>
+
+            <h1>Maybe you just want the best all around!</h1>
+            <a>- https://shorturl.at/TWQ2T</a>
+            </div>
+            </html>
+        ): showCreateUser ? (
+          <body>
+              <h1>YOYOYO</h1>
+          </body>
+        ): null}
+      </main>
     </div>
-
-<div>
-    <head>
-
-    <link rel="stylesheet" href="style.css"></link>
-    <title> BNBJI Financing</title>
-    
-    </head>
-</div>
-
-    <body>
-      <form>
-
-      <div>
-      <p> Login</p>
-      <label htmlFor="user">Username:</label>
-      <input type="text" id="user" placeholder=" Please Type Here" required/>
-      </div>
-
-      <div>
-      <label htmlFor="pass">Password:</label>
-      <input type="text" id="pass" placeholder=" Please Type Here" required/>
-      </div>
-
-      <div>
-      <p><label htmlFor="submit">Submit</label></p>
-      <input type="button" id="submit"/>
-      </div>
-
-      <div>
-      <p>Don't have an account? <label htmlFor="create">Create User</label></p>
-      <input type="button" id="create"/>
-      </div>
-
-      <div>
-      <label htmlFor="forgot">Forgot Password</label>
-      <input type="button" id="forgot"/>
-      </div>
-      
-      </form>
-    </body>
-
-    </>
-
-  )
-
+  );
 }
