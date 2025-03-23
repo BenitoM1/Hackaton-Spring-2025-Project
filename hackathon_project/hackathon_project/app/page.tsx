f24452796ee50972186bfe5d52f3cd8e0776485d
Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>
2025-03-23 11:51:24 -0500

    1234
    Signed-off-by: Benito Mendoza <133236928+BenitoM1@users.noreply.github.com>

--- hackathon_project/hackathon_project/app/page.tsx
+++ hackathon_project/hackathon_project/app/page.tsx
@@ -8,23 +8,70 @@ export default function Home() {
   const [showHome, setShowHome] = useState(false)
   const [showBudgetHow, setShowBudgetHow] = useState(false)
   const [showCreateUser, setShowCreateUser] = useState(false)
+  const [showStock, setShowStock] = useState(false)
+  const [showStockHow, setStockHow] = useState(false)
+  const [showAddSub, setAddSub] = useState(false)
+  
+
+  const toggleAddSub = () => {
+    setAddSub(true)
+    setShowHome(false)
+    setShowBudgetHow(false)
+    setShowBudgeting(false)
+    setShowStock(false)
+    setStockHow(false)
+    
+  }
+
+  const toggleStockHow = () => {
+    setShowHome(false)
+    setShowBudgetHow(false)
+    setShowBudgeting(false)
+    setShowStock(false)
+    setStockHow(true)
+    setAddSub(false)
+
+  }
+
+  const toggleStock = () => {
+    setShowHome(false)
+    setShowBudgetHow(false)
+    setShowBudgeting(false)
+    setShowStock(true)
+    setStockHow(false)
+    setAddSub(false)
+
+  }
 
   const toggleBudget = () => {
     setShowHome(false)
     setShowBudgetHow(false)
     setShowBudgeting(true)
+    setShowStock(false)
+    setStockHow(false)
+    setAddSub(false)
+
+
   }
 
   const toggleHome = () => {
     setShowBudgeting(false)
     setShowBudgetHow(false)
     setShowHome(true)
+    setShowStock(false)
+    setStockHow(false)
+    setAddSub(false)
+
+
   }
 
   const toggleBudgetHow = () => {
     setShowBudgeting(false)
     setShowHome(false)
     setShowBudgetHow(true)
+    setShowStock(false)
+    setStockHow(false)
+
   }
 
   const toggleCreateUser = () => {
@@ -39,11 +86,11 @@ export default function Home() {
         </div>
 
         <div>
-          <Link href="/addsub" className="button-class">Add/Sub</Link>
+        <button onClick={toggleAddSub} className="button-class"> Add/Sub </button>
         </div>
 
         <div>
-          <Link href="/stocks" className="button-class">Stocks</Link>
+        <button onClick={toggleStock} className="button-class"> Stock </button>
         </div>
 
         <div>
@@ -56,7 +103,7 @@ export default function Home() {
       </head>
 
       <main className="main">
-        {!showBudgeting && !showHome ? (
+        {!showBudgeting && !showHome && !showAddSub && !showBudgetHow && !showCreateUser && !showStock && !showStockHow? (
           <form>
             <div>
               <label htmlFor="user">Username:</label>
@@ -102,9 +149,11 @@ export default function Home() {
             </div>
           </div>
         ): showHome ? (
-          <body>
-            
-          </body>
+          <html>
+            <div>
+              <h1>Hey guys! This is the lovely home page. There was meant to be a graph and you total money here. Just imagine it.</h1>
+            </div>
+          </html>
 
         ): showBudgetHow ?(
           <html>
@@ -123,9 +172,56 @@ export default function Home() {
             </div>
             </html>
         ): showCreateUser ? (
+          <div className="signup-container">
+        <h1>Create an Account</h1>
+        <form action="/signup" method="POST">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+  
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+  
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+  
+          <label htmlFor="checking">Checking Account Number</label>
+          <input type="text" id="checking" name="checking" required />
+  
+          <label htmlFor="savings">Savings Account Number</label>
+          <input type="text" id="savings" name="savings" required />
+  
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+        ): showAddSub ?(
           <body>
-              <h1>YOYOYO</h1>
+            <h1> testing for addsub</h1>
           </body>
+        ): showStock ?(
+          <body>
+            <h1>testing</h1>
+            <button onClick={toggleStockHow} className="button-class"> How To use Stocks </button>
+          </body>
+
+        ): showStockHow ?(
+          <html>
+          <div>
+          <h1>Stock Guide</h1>
+
+          <h1>It Looks Like You Might Need Help With Stocks.</h1>
+
+          <h1>Here is a good option for safety.</h1>
+          <a>- "https://shorturl.at/mHvjM"</a>
+
+          <h1>Looking for something user friendly?</h1>
+          
+          <a>- "https://shorturl.at/goNYG"</a>
+
+          <h1>Maybe you just want the best all around!</h1>
+          <a> "https://shorturl.at/CSTp6"</a>
+
+          </div>
+          </html>
         ): null}
       </main>
     </div>
